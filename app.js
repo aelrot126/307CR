8e7864256cfb012ad958a066264550a6286f444d
Jaden <yjian126@hotmail.com>
2018-11-27 18:24:59 +0800

    hello

--- app.js
+++ app.js
@@ -84,6 +84,6 @@ server.get('/deleteall', (req, res) => {
 });
 
 
-server.listen(5000, () => {
-  console.log('server started on port 5000');
+server.listen(3000, () => {
+  console.log('server started on port 3000');
 });
